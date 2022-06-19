fdcca9c95a263a13c4c2eda8d4bba97bb8da6f3b
mathiskrr <94641551+mathiskrr@users.noreply.github.com>
2022-06-19 11:33:26 +0200

    Changement Titre

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
     <!--*****************NAVBAR*****************-->
      <nav class="navbar navbar-expand-lg navbar-light bg-light"> 
     	<a class="navbar-brand" href="#haut">
-    		<img src="img/logodax.png"></a> <div class="navbarh1"><h1>Salles de Travail - Salle <?php echo $salle?></h1></div>
+    		<img src="img/logodax.png"></a> <div class="navbarh1"><h1>Test -Salles de Travail - Salle <?php echo $salle?></h1></div>
     	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       		<span class="navbar-toggler-icon"></span>
     	</button>
